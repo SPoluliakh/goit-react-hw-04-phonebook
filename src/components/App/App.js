4455042990c74e6c75e644c6419e1d0a6a2b36db
SPoluliakh <S_Poluliakh@ukr.net>
2022-11-01 22:09:00 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from '../Form';
 import ContactList from '../ContactList';
@@ -14,14 +14,14 @@ export const App = () => {
     const initialValue = JSON.parse(saved);
     return (
       initialValue || [
-        { id: 'id-1', name: 'Rosie Simpson (test)', number: '459-12-56' }, // test
-        { id: 'id-2', name: 'Hermione Kline (test)', number: '443-89-12' }, // test
-        { id: 'id-3', name: 'Eden Clements (test)', number: '645-17-79' }, // test
-        { id: 'id-4', name: 'Annie Copeland (test)', number: '227-91-26' }, // test
-        { id: 'id-5', name: 'Anna Sim (test)', number: '459-12-56' }, // test
-        { id: 'id-6', name: 'Hermi Kla (test)', number: '443-89-12' }, // test
-        { id: 'id-7', name: 'Eduard Mendi (test)', number: '645-17-79' }, // test
-        { id: 'id-8', name: 'Nataly Iqwel (test)', number: '227-91-26' }, // test
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' }, // test
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' }, // test
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' }, // test
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }, // test
+        { id: 'id-5', name: 'Anna Sim', number: '459-12-56' }, // test
+        { id: 'id-6', name: 'Hermi Kla', number: '443-89-12' }, // test
+        { id: 'id-7', name: 'Eduard Mendi', number: '645-17-79' }, // test
+        { id: 'id-8', name: 'Nataly Iqwel', number: '227-91-26' }, // test
       ]
     );
   });
@@ -54,14 +54,14 @@ export const App = () => {
   };
 
   //Responsible for rendering the requested/all contacts
-  const findContactbyName = () => {
+  const findContactbyName = useMemo(() => {
     const fiterNameToLowerCase = filter.toLowerCase();
     const findContactsbyName = contacts.filter(contact =>
       contact.name.toLowerCase().includes(fiterNameToLowerCase)
     );
 
     return findContactsbyName;
-  };
+  }, [filter, contacts]);
 
   //Responsible for deleting contacts
   const deletContact = idx => {
@@ -88,119 +88,11 @@ export const App = () => {
         <ContactForm onSubmit={addContact} />
         <Filter filter={filter} onChange={handleInputChange} />
         <ContactList
-          contactsQnt={findContactbyName().length}
-          renderItems={findContactbyName()}
+          contactsQnt={findContactbyName.length}
+          renderItems={findContactbyName}
           onDelitBtn={deletContact}
         />
       </Box>
     </Box>
   );
 };
-
-// import React, { Component } from 'react';
-// import { nanoid } from 'nanoid';
-// import ContactForm from '../Form';
-// import ContactList from '../ContactList';
-// import Filter from '../Filter';
-// import { Box } from '../../components/Box';
-// import { MaineTitle } from './App.styled';
-
-// export class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson (test)', number: '459-12-56' }, // test
-//       { id: 'id-2', name: 'Hermione Kline (test)', number: '443-89-12' }, // test
-//       { id: 'id-3', name: 'Eden Clements (test)', number: '645-17-79' }, // test
-//       { id: 'id-4', name: 'Annie Copeland (test)', number: '227-91-26' }, // test
-//       { id: 'id-5', name: 'Anna Sim (test)', number: '459-12-56' }, // test
-//       { id: 'id-6', name: 'Hermi Kla (test)', number: '443-89-12' }, // test
-//       { id: 'id-7', name: 'Eduard Mendi (test)', number: '645-17-79' }, // test
-//       { id: 'id-8', name: 'Nataly Iqwel (test)', number: '227-91-26' }, // test
-//     ],
-//     filter: '',
-//   };
-
-//   LSK_CONTACTS = 'myContacts';
-//   componentDidMount() {
-//     const contactList = localStorage.getItem(this.LSK_CONTACTS);
-//     if (JSON.parse(contactList)) {
-//       this.setState({ contacts: JSON.parse(contactList) });
-//     }
-//   }
-
-//   componentDidUpdate(prevState) {
-//     const oldContacts = prevState.contacts;
-//     const newContacts = this.state.contacts;
-//     if (oldContacts !== newContacts) {
-//       localStorage.setItem(this.LSK_CONTACTS, JSON.stringify(newContacts));
-//     }
-//   }
-
-//   // Responsible for updating the state
-//   handleInputChange = evt => {
-//     const { name, value } = evt.currentTarget;
-//     this.setState({ [name]: value });
-//   };
-
-//   // Add new contacts, inform if contact already added, updating the state
-//   addContact = (name, number) => {
-//     const addName = this.state.contacts.map(contact => contact.name);
-//     addName.includes(name);
-//     if (addName.includes(name)) {
-//       return alert(`${name} is already in contacts`);
-//     }
-//     const contact = {
-//       id: nanoid(),
-//       name,
-//       number,
-//     };
-//     this.setState(prevState => ({
-//       contacts: [contact, ...prevState.contacts],
-//     }));
-//   };
-
-//   //Responsible for rendering the requested/all contacts
-//   findContactbyName = () => {
-//     const { filter, contacts } = this.state;
-//     const fiterNameToLowerCase = filter.toLowerCase();
-//     const findContactsbyName = contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(fiterNameToLowerCase)
-//     );
-
-//     return findContactsbyName;
-//   };
-
-//   //Responsible for deleting contacts
-//   deletContact = idx => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(({ id }) => id !== idx),
-//     }));
-//   };
-
-//   render() {
-//     const { filter } = this.state;
-//     const renderContactsList = this.findContactbyName();
-//     return (
-//       <Box display="flex">
-//         <Box
-//           marginLeft="auto"
-//           marginRight="auto"
-//           padding={4}
-//           border="phonebook"
-//           backgroundColor="phonebookBcg"
-//           boxShadow="boxShadow"
-//           minWidth="420px"
-//         >
-//           <MaineTitle>Phonebook</MaineTitle>
-//           <ContactForm onSubmit={this.addContact} />
-//           <Filter filter={filter} onChange={this.handleInputChange} />
-//           <ContactList
-//             contactsQnt={renderContactsList.length}
-//             renderItems={renderContactsList}
-//             onDelitBtn={this.deletContact}
-//           />
-//         </Box>
-//       </Box>
-//     );
-//   }
-// }
